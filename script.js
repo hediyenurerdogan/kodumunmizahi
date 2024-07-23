dbaa3b98134129aaa2c77cfb624f5abf3bc2b9c0
hediyenurerdogan <hediyenurerdogann@gmail.com>
2024-07-23 04:19:29 +0300

    güncelleme
    güncelleme

--- script.js
+++ script.js
@@ -1,65 +1,47 @@
-let submitBtn = document.getElemtById("submit-btn");
+document.addEventListener("DOMContentLoaded", function () {
+    let submitBtn = document.getElementById("submit-btn");
 
-let generateGif = () => {
-    let loader= document.querySelector(".loader");
-    loader.style.display ="block";
-    document.querySelector(".wrapper").style.display="none";
-    let q = document.getElementById("search-box").value;
+    let generateCards = () => {
+        let loader = document.querySelector(".loader");
+        loader.style.display = "block";
+        document.querySelector(".wrapper").style.display = "none";
+        
+        // Resim dosya adları ve başlıkları
+        let images = [
+            { src: "img1.jpg", title: "Card 1" },
+            { src: "img2.jpg", title: "Card 2" },
+            { src: "img3.jpg", title: "Card 3" },
+            { src: "img4.jpg", title: "Card 4" },
+            { src: "img5.jpg", title: "Card 5" },
+            { src: "img6.jpg", title: "Card 6" },
+            { src: "img7.jpg", title: "Card 7" },
+            { src: "img8.jpg", title: "Card 8" },
+            { src: "img9.jpg", title: "Card 9" },
+            { src: "img10.jpg", title: "Card 10" }
+        ];
 
-    let gifCount=10;
-    let finalURL = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}&limit=${gifCount}&offset=0&rating=q&lang=en`;
-    document.querySelector(".wrapper").innerHTML ="";
+        let imageCount = images.length;
+        document.querySelector(".wrapper").innerHTML = "";
 
-    fetch(finalURL)
-        .then((resp) => resp.json())
-        .then((info) => {
-            console.log(info.data);
-            let gifsData = info.data;
-            gifsData.forEach((gif) =>  {
-
-                let container= document.createElement("div");
-                container.classList.add("container");
-                let iframe = document.createElement("img");
-                console.log(gif);
-                iframe.setAttribute("src",gif.images.downsized_medium.url);
-                iframe.onload = () => {
-                    gifCount--;
-                    if (gifCount == 0) {
-                        loader.style.display = "none";
-                        document.querySelector(".wrapper").style.display ="grid";
-
-                    }
-                };
-                container.append(iframe);
-                let copyBtn = document.createElement("button");
-                copyBtn.innerText = "Copy Link";
-                copyBtn.onclick = () => {
-                    let copyLink = "https:"//media4.giphy.com/media/${gif.id}/giphy.mp4";
-
-                    navigator.clipboard
-                        .writeText(copyLink)
-                        .then(() =>  {
-                            alert("GIF copied to clipboard");
-                        })
-                        .catch(() => {
-                            alert("GIF copied to clipboard");
-                            let hiddenInput = document.createElement("input");
-                            hiddenInput.setAttribute("type","text");
-                            document.body.appendChild(hiddenInput);
-                            hiddenInput.value = copyLink;
-                            hiddenInput.select();
-                            document.execCommand("copy");
-                            document.body.removeChild(hiddenInput);
-                            });
-            container.append(copyBtn);
-            document.querySelector(".wrapper").append
+        images.forEach((image) => {
+            let container = document.createElement("div");
+            container.classList.add("card");
+            let img = document.createElement("img");
+            img.setAttribute("src", `path/to/img/${image.src}`);
+            img.onload = () => {
+                imageCount--;
+                if (imageCount === 0) {
+                    loader.style.display = "none";
+                    document.querySelector(".wrapper").style.display = "grid";
                 }
-                document.querySelector(".wrapper"),append (container);
-
-            } )
+            };
+            let title = document.createElement("h3");
+            title.innerText = image.title;
+            container.append(img);
+            container.append(title);
+            document.querySelector(".wrapper").append(container);
         });
+    };
 
-};
-
-submitBtn.addEventListener("click",generateGif);
-window.addEventListener("load",generateGif);
+    submitBtn.addEventListener("click", generateCards);
+});
